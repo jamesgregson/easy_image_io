53ea93211a5dae1933f9a6bb5ab3e3244cd6d584
james gregson <james.gregson@gmail.com>
2017-04-30 09:36:17 -0700

    added conda prefix environment variable

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from sys import platform
+import os
 
 libs = []
-if platform =='linux':
-    anaconda_dir = '/anaconda'
-elif platform == 'win32':
-    anaconda_dir = 'C:/Program Files/Anaconda3'
-    libs = [ 'shell32' ]
-elif platform == 'darwin':
-    anaconda_dir = '/anaconda'
+anaconda_dir = os.environ['CONDA_PREFIX']
+# if platform =='linux':
+#     anaconda_dir = '/anaconda'
+# elif platform == 'win32':
+#     anaconda_dir = 'C:/Program Files/Anaconda3'
+#     libs = [ 'shell32' ]
+# elif platform == 'darwin':
+#     anaconda_dir = '/anaconda'
 
 png_include_dir  = '%s/include' % anaconda_dir
 png_library_dir  = '%s/lib' % anaconda_dir
