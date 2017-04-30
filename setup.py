f98c5fed1450d06d667c99f5f202023542a22616
james gregson <james.gregson@gmail.com>
2017-04-30 13:07:37 -0700

    tweaks to setup

--- setup.py
+++ setup.py
@@ -9,17 +9,20 @@ def find(name, path):
             return os.path.join(root, name)
     return '';
 
+print('locating directories...')
 defines      = [ ('MAJOR_VERSION',0),('MINOR_VERSION',1) ]
 include_dirs = [ numpy.get_include() ]
-libraries    = config.libs
+libraries    = []
 library_dirs = []
 
+print('checking for tiffio.h...')
 if find('tiffio.h', config.tiff_include_dir) != '':
     defines.append( ('cimg_use_tiff',1) )
     include_dirs.append( config.tiff_include_dir )
     libraries.append( 'tiff' )
     library_dirs.append( config.tiff_library_dir )
 
+print('checking for png.h...')
 if find('png.h', config.png_include_dir ) != '':
     defines.append( ('cimg_use_png',1) )
     include_dirs.append( config.png_include_dir )
@@ -29,6 +32,7 @@ if find('png.h', config.png_include_dir ) != '':
 for lib in config.libs:
     libraries.append( lib )
 
+print('Setting up extension...')
 easy_image_io = Extension('easy_image_io',
                     define_macros=defines,
                     sources=['easy_image_io.cpp'],
@@ -36,4 +40,5 @@ easy_image_io = Extension('easy_image_io',
                     library_dirs=library_dirs,
                     libraries=libraries )
 
+print('Building extension...')
 setup(name='easy_image_io', version='0.1', ext_modules=[ easy_image_io ] )
